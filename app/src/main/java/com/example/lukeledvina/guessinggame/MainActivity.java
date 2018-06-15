f12eb64a3545cf4b307ae7a32ac7d1e505204897
Luke Ledvina <soulgp1@gmail.com>
2018-06-15 13:37:22 -0400

    Completed logic for main activity

--- app/src/main/java/com/example/lukeledvina/guessinggame/MainActivity.java
+++ app/src/main/java/com/example/lukeledvina/guessinggame/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.lukeledvina.guessinggame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.start_button);
+
+        setListener();
+    }
+
+    private void setListener() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startGame();
+            }
+        });
+
+
+    }
+
+    //Method to handle the intent to go from this activity to the game activity
+    private void startGame() {
+
+        Intent playGame = new Intent(this, GameActivity.class);
+        startActivity(playGame);
+
     }
 }
